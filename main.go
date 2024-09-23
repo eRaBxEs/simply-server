7b484a3e82801b062db8f63945d59db98f1b1075
Ehioje Henry Erabor <ehiojee@yahoo.com>
2024-09-23 07:01:02 +0100

    add request for array of names and then respond with an array of greetings in json

--- main.go
+++ main.go
@@ -11,6 +11,14 @@ type Response struct {
 	Message string `json:"message"`
 }
 
+type GreetingRequest struct {
+	Names []string `json:"names"`
+}
+
+type GreetingResponse struct {
+	Greetings []string `json:"greetings"`
+}
+
 func greetHandler(w http.ResponseWriter, r *http.Request) {
 	response := Response{Message: "Hello"}
 	w.Header().Set("Content-Type", "application/json")
@@ -24,10 +32,35 @@ func greetByNameHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(response)
 }
 
+func greetAllNamesHandler(w http.ResponseWriter, r *http.Request) {
+	// http verb method check
+	if r.Method != http.MethodPost {
+		http.Error(w, "Only POST request are accepted!", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// process request
+	var req GreetingRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "Invalied request body", http.StatusBadRequest)
+		return
+	}
+
+	var res GreetingResponse
+
+	for _, name := range req.Names {
+		res.Greetings = append(res.Greetings, fmt.Sprintf("Hello, %s!", name))
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(res)
+}
+
 func main() {
 	router := http.NewServeMux()
 	router.HandleFunc("/", greetHandler)
 	router.HandleFunc("/you", greetByNameHandler)
+	router.HandleFunc("/greet", greetAllNamesHandler)
 
 	server := http.Server{
 		Addr:    ":8080",
