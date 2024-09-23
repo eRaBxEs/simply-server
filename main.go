b0ee57dba86bb3415788037008363069bc3afaee
Ehioje Henry Erabor <ehiojee@yahoo.com>
2024-09-23 06:12:36 +0100

    accept data in query parameter and return json response

--- main.go
+++ main.go
@@ -17,9 +17,17 @@ func greetHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(response)
 }
 
+func greetByNameHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	response := Response{Message: fmt.Sprintf("Hello, %s", name)}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
 func main() {
 	router := http.NewServeMux()
 	router.HandleFunc("/", greetHandler)
+	router.HandleFunc("/you", greetByNameHandler)
 
 	server := http.Server{
 		Addr:    ":8080",
